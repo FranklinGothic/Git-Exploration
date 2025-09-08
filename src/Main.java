4161375019b79e7a768674a2c0383b3988fc3a3b
127AEberhardt <127Aeberhardt@frhsd.com>
2025-09-08 10:56:01 -0400

    ifisdafodgjkaoriginal

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         String name = "Alex";
         System.out.println("Hi my name is " + name);
+        System.out.println("This was my original");
     }
 }
