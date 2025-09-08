14f213083f1853edf756169040f4a1e3682a0924
127AEberhardt <127Aeberhardt@frhsd.com>
2025-09-08 10:15:54 -0400

    name

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name = "Alex";
+        System.out.println("Hi my name is " + name);
     }
 }
