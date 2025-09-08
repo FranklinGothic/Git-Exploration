eadef15991653767ec2d4851026c18faed68c7de
nulledbird <abirdcoder.dev@gmail.com>
2025-09-08 10:34:28 -0400

    rishabh

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         String name = "Alex";
+        String other_person = "Rishabh";
         System.out.println("Hi my name is " + name);
+        System.out.println("Hi, I'm " + other_person);
     }
 }
