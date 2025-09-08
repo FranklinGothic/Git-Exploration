81346a6eb8daea2da5e2b1dc005ffc555da57bdc
127ABhatt <127abhatt@frhsd.com>
2025-09-08 10:54:42 -0400

    anay

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         String name = "Alex";
         String other_person = "Rishabh";
+        String other_other_person = "Anay";
         System.out.println("Hi my name is " + name);
         System.out.println("Hi, I'm " + other_person);
+        System.out.println("Hi, I'm " + other_other_person);
     }
 }
